2dec0e705c93c18cc11b12df1429eb6f81e5d915
Dan Rice <dan.rice.92@outlook.com>
2017-07-19 17:04:35 -0600

    Databases Assignment 5 Error Handling tweak

--- lib/bloc_record/error_handling.rb
+++ lib/bloc_record/error_handling.rb
@@ -19,26 +19,3 @@ module ErrorHandling
     end
   end
 end
-#
-# module ErrorHandling
-#   def method_missing(method_name, *args)
-#     puts args
-#     if method_name.match(/find_by_/)
-#       attribute = method_name.to_s.split('find_by_')[1]
-#       if columns.include?(attribute)
-#         self.find_by(attribute, args.first)
-#       else
-#         puts "#{attribute} does not exist in the database -- please try again."
-#       end
-#     elsif method_name.match(/update_/)
-#       puts "update path"
-#       puts args
-#       # attribute = method_name.to_s.split('update_')[1]
-#       # p attribute
-#       # self.update_attribute(self.id, { attribute => args.last } )
-#     else
-#       puts "super path"
-#       super
-#     end
-#   end
-# end
